af08c0181694ad2e3dd3bb5c7511562d55fe93aa
Władysław Cynkiewicz <vladar3vvv@yahoo.com>
2026-03-30 20:13:02 +0200

    added time to core

--- core.py
+++ core.py
@@ -5,9 +5,12 @@ Created on Sat Mar  7 15:50:01 2026
 @author: Wlad
 """
 
+
+import time 
+start_time = time.time()
 #print("Enter file name with path")
 #tekfil=input()
-tekfil="C:\\nowy 1.txt"
+tekfil="nowy 1.txt"
 print(f"Words from {tekfil} will be counted")
 
 with open(tekfil, 'r') as file:
@@ -18,3 +21,4 @@ print(zawar)
 casedown = [i.lower() for i in (zawar.replace(".","").split())]
 print(f"Total number of words: {len(casedown)}")
 print(f"Number of unique words: {len(set(casedown))}")
+print("--- %s seconds ---" % (time.time() - start_time))
