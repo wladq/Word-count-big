725ad5daf2b9ecf3e57ffd4bcfbc89986904b091
Władysław Cynkiewicz <vladar3vvv@yahoo.com>
2026-04-07 17:01:03 +0200

    logging messages file

---
+++ wocolog.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 15:17:40 2026
+
+@author: Wlad
+"""
+
+import logging 
+logger = logging.getLogger(__name__)
+
+def notfounderr():
+    logging.error("Filename hasn't been provided")
+    print("Please provide a filename in the argument.")
+    
+def permerr():
+    logging.error("Permission to the file or path denied by the system.")
+    print("Permission to the file or path denied by the system.")
+
+def incorerr():
+    logging.error("Incorrect filename or filename not found")
+    print("Please provide a correct filename or filename with a path. If the filename contains spacing, it should be quoted.")
