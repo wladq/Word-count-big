31976198e93e5fb37f718aae300e03e5b5a9dc75
Władysław Cynkiewicz <vladar3vvv@yahoo.com>
2026-04-10 17:26:55 +0200

    added infos with variables to module

--- wocolog.py
+++ wocolog.py
@@ -19,3 +19,19 @@ def permerr():
 def incorerr():
     logging.error("Incorrect filename or filename not found")
     print("File not found or incorrect. Please provide a correct filename or filename with a path. If the filename contains spacing, it should be quoted.")
+
+
+    
+def fileusedinfo(funfil):
+    logging.info(f"Words from {funfil} will be counted")
+    
+def starttimeinfo(sttim):
+    logging.info(f'Program started at {sttim}')
+    
+def endtimeanddurationinfo(sttim,tim):
+    logging.info(f'Program finished at {tim}')
+    logging.info(f'--- {(tim - sttim)} seconds ---' )
+    
+def countednumberinfo(numcount):
+    logging.info(f"Number of counted words: {numcount}")
+    
