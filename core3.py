ca1e88276675a9e1e8008461e276b35cf869fa1d
Władysław Cynkiewicz <vladar3vvv@yahoo.com>
2026-04-22 10:20:15 +0200

    renamed from wocolog and applied notes from meeting

--- core3.py
+++ core3.py
@@ -5,10 +5,14 @@ Created on Fri Apr 10 16:56:18 2026
 @author: Wlad
 """
 
-import sys
+import argparse
 import logging
-import wocolog
+import logging_helpers
+from pathlib import Path 
 import time
+#import sys
+
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
@@ -17,10 +21,6 @@ logging.basicConfig(
     filename="Info-logs.log")
 
 start_tim = time.time()
-
-def main() -> None:
-    wocolog.starttimeinfo(start_tim)
-
 
 def word_count(filepath):
     words=set()
@@ -38,19 +38,32 @@ def word_count(filepath):
                     for word in subline.split():
                         words.add(word)
 
-    wocolog.fileusedinfo(filepath)
+    logging_helpers.fileusedinfo(filepath)
     return len(words)
 
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Count unique words in a file."
+    )
+    parser.add_argument(
+        "filepath",
+        type=Path,
+        help="Path to the input text file"
+    )
+    args = parser.parse_args()
+    start_time = time.time()
+    logging_helpers.startTimeInfo(start_time)
+    try:
+        wordCount = word_count(args.filepath)
+        print(wordCount)
+        logging_helpers.countedNumberInfo(wordCount)
+    except IndexError:
+        logging_helpers.notFoundError()
+    except PermissionError:
+        logging_helpers.logPermissionError()
+    except FileNotFoundError:
+        logging_helpers.logIncorrectError()
+    logging_helpers.endTimeAndDurationInfo(start_time, time.time())
 
 if __name__ == "__main__":
-    try:
-        numw = word_count(sys.argv[1])
-        print(numw)
-        wocolog.countednumberinfo(numw)
-    except IndexError:
-        wocolog.notfounderr()
-    except PermissionError:
-        wocolog.permerr()
-    except FileNotFoundError:
-        wocolog.incorerr()
-    wocolog.endtimeanddurationinfo(start_tim, time.time())
+    main()
